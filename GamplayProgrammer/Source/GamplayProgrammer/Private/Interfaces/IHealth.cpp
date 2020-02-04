edf4b204a9b51dca1fcdaf4a2ee296ab54922907
Víctor de Castro Hurtado <victor.castro@live.u-tad.com>
2020-02-04 17:11:43 +0100

    New folder system

---
+++ GamplayProgrammer/Source/GamplayProgrammer/Private/Interfaces/IHealth.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "IHealth.h"
+
+// Add default functionality here for any IIHealth functions that are not pure virtual.
